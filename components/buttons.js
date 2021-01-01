6b6cd78ee51857201d3775559435461ab3e5ce28
John Law <johnlaw.po@gmail.com>
2021-01-02 02:05:25 +0800

    separate styles

--- components/buttons.js
+++ components/buttons.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Buttons.module.css';
 
 const Buttons = (props) => {
   const { clearDesk, applyWaterMark } = props;
