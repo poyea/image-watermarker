8fe4b0f2a302b9d215e27f85d8b05a093c782549
John Law <johnlaw.po@gmail.com>
2021-01-13 23:58:12 +0800

    fix: add title

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -5,14 +5,26 @@ const Buttons = (props) => {
   const { clearDesk, applyWaterMark, downloadImages } = props;
   return (
     <div className={styles.buttons}>
-      <button className={styles.button} onClick={clearDesk}>
+      <button
+        className={styles.button}
+        onClick={clearDesk}
+        title="Clear the page."
+      >
         X
       </button>
       {/* <button className={styles.button}>+</button> */}
-      <button className={styles.button} onClick={applyWaterMark}>
+      <button
+        className={styles.button}
+        onClick={applyWaterMark}
+        title="Apply watermarks."
+      >
         A
       </button>
-      <button className={styles.button} onClick={downloadImages}>
+      <button
+        className={styles.button}
+        onClick={downloadImages}
+        title="Download the edited images."
+      >
         D
       </button>
     </div>
